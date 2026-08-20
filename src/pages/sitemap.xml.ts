6adba12bd9f89c7b334d7598428c7cd40776ef0e
Jinank Thakker <jinankthakker@gmail.com>
2026-08-19 23:07:06 -0400

    Add startup ecosystem pages and full width footer

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -4,7 +4,7 @@ import { getApps } from '../lib/apps';
 export const GET: APIRoute = ({ site }) => {
   const base = site!.toString().replace(/\/$/, '');
   const apps = getApps();
-  const urls = ['', '/directory', '/tools', '/open-source', '/showcase', '/sell', ...apps.map((app) => `/${app.slug}`)];
+  const urls = ['', '/directory', '/company-directory', '/news', '/jobs', '/resources', '/partner', '/tools', '/open-source', '/showcase', '/sell', ...apps.map((app) => `/${app.slug}`)];
   const xml = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${urls.map((path) => `\n  <url><loc>${base}${path}</loc><changefreq>${path ? 'monthly' : 'weekly'}</changefreq><priority>${path ? '0.8' : '1.0'}</priority></url>`).join('')}\n</urlset>`;
   return new Response(xml, { headers: { 'Content-Type': 'application/xml; charset=utf-8' } });
 };
